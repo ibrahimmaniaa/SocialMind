d82777ba835c130e17e1043652d49f370d6bc23d
ibrahimmaniaa94 <ebrahimmm1994@gmail.com>
2024-11-30 22:29:22 +0100

    removed unused directives

--- src/SocialMind.Core/LLMApiServices/ILanguageModelService.cs
+++ src/SocialMind.Core/LLMApiServices/ILanguageModelService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SocialMind.Core.LLMApiServices
+﻿namespace SocialMind.Core.LLMApiServices
 {
     public interface ILanguageModelService
     {
