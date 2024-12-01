fa0dc879d936a2cf440d627277165e6de6251790
ibrahimmaniaa94 <ebrahimmm1994@gmail.com>
2024-12-01 22:28:41 +0100

    Added Gemini configuration

---
+++ src/SocialMind.Core/Domain/GeminiConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace SocialMind.Core.Domain;
+
+/// <summary>
+/// Gemini configuration
+/// </summary>
+public class GeminiConfig
+{
+    /// <summary>
+    /// Entire request uri for generating content
+    /// </summary>
+    public static string RequestUri => $"{BaseUrl}{Model}{Endpoint}";
+
+    private static string BaseUrl => "https://generativelanguage.googleapis.com/v1beta/models/";
+
+    private static string Model => "gemini-1.5-flash";
+
+    private static string Endpoint => ":generateContent";
+}
