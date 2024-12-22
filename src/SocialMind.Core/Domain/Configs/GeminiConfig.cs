52164e41f91ff577598bb510f6d094305e7d8c65
ibrahimmaniaa94 <ebrahimmm1994@gmail.com>
2024-12-22 18:44:26 +0100

    Added config intrerface and extended gemini config

--- src/SocialMind.Core/Domain/Configs/GeminiConfig.cs
+++ src/SocialMind.Core/Domain/Configs/GeminiConfig.cs
@@ -3,21 +3,16 @@
 /// <summary>
 /// Gemini configuration
 /// </summary>
-public class GeminiConfig
+public class GeminiConfig : ILanguageModelClientConfig
 {
-    /// <summary>
-    /// Entire request uri for generating content
-    /// </summary>
-    public static string RequestUri => $"{BaseUrl}{Model}{Endpoint}";
-
-    /// <summary>
-    /// Authorization API key
-    /// </summary>
-    public static string ApiKey => "";
+    /// <inheritdoc />
+    public string RequestUri => $"{BaseUrl}{Model}{Endpoint}?key={ApiKey}";
 
     private static string BaseUrl => "https://generativelanguage.googleapis.com/v1beta/models/";
 
     private static string Model => "gemini-1.5-flash";
 
     private static string Endpoint => ":generateContent";
+
+    private static string ApiKey => "";
 }
